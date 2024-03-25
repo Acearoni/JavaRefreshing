b7f19e4c1ea178e2efef70f745e80ace842563f9
adam woodson <awoodson.contact@gmail.com>
2024-03-25 15:45:23 -0500

    first commit

---
+++ src/main/java/com/self/refresher/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.self.refresher;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaRefresherApplication.class);
+	}
+
+}
---
+++ src/test/java/com/self/refresher/JavaRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
