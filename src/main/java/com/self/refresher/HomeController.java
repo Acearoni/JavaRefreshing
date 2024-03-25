45f6f7d522141e3264f084222a9d9fccd7c64ac8
adam woodson <awoodson.contact@gmail.com>
2024-03-25 15:55:08 -0500

    Controller Added

---
+++ src/main/java/com/self/refresher/HomeController.java
@@ -0,0 +1,16 @@
+package com.self.refresher;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HomeController {
+	@RequestMapping("/")
+	public String index(){
+		return "Hello World";
+	}
+	@RequestMapping("/world")
+	public String world( ) {
+		return "Class annotations are cool too";
+	}
+}
